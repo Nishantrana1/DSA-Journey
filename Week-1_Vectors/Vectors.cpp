add52372fa7313b42a89db87eb2127e406da37f3
Nishant Rana <94187nishant@gmail.com>
2026-01-10 22:30:12 +0530

    Some experiment

--- Week-1_Vectors/Vectors.cpp
+++ Week-1_Vectors/Vectors.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+int main() {
+    vector<int> v;   // declare vector
 
-int main(){
-    int n;
-    cout<<"Hello";
-    cin>>n;
-    cout<<n;
+    // insert elements
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+
+    // access elements
+    cout << "Elements: ";
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+
     return 0;
 }
