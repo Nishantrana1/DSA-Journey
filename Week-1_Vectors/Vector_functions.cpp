0ede5fe59cb22f404e6fa8f8ee612e8cab77fc93
Nishant Rana <94187nishant@gmail.com>
2026-01-11 21:34:50 +0530

    Vector function

--- Week-1_Vectors/Vector_functions.cpp
+++ Week-1_Vectors/Vector_functions.cpp
@@ -9,11 +9,16 @@ int main(){
     // size of vector
     cout<<"Vector size = "<< vec.size()<<endl;
 
-
+cout<<vec.front()<<endl;
+cout<<vec.back()<<endl;
 vector<int> hey;
 // for adding the value
 hey.push_back(25);
+hey.push_back(35);
+hey.push_back(45);
+hey.push_back(55);
 cout<<hey.size()<<endl;
+hey.pop_back();
 for (int val:hey){
     cout<<val<<" ";
 }
