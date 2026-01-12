57b11c0bef1eec8a09febae2820466815d89c165
Nishant Rana <94187nishant@gmail.com>
2026-01-12 23:14:05 +0530

    try  Kadane's Algorithm

--- Week-1_Vectors/max_subarray_sum.cpp
+++ Week-1_Vectors/max_subarray_sum.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -16,4 +17,25 @@ for (int st=0;st<n;st++){
 cout<<endl;
 }
 return 0;
+}*/
+
+
+
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    int currentSum = 0;
+    int maxSum = INT_MIN;
+
+    for (int i = 0; i < arr.size(); i++) {
+        currentSum = max(arr[i], currentSum + arr[i]);
+        maxSum = max(maxSum, currentSum);
+    }
+
+    cout << "Maximum Subarray Sum = " << maxSum;
 }
