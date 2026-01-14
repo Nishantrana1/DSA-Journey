43ab567cd145ff0a66c6b1ec9848ba3477a51888
Nishant Rana <94187nishant@gmail.com>
2026-01-14 22:28:37 +0530

    majority_element using the 0(nlogn) time complexity

--- Week-1_Vectors/majority_element.cpp
+++ Week-1_Vectors/majority_element.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<vector>
 using namespace std;
 class Solution {
@@ -29,4 +29,41 @@ int main(){
 vector<int> nums={2,2,1,1,1,1,2};
 int ans=s.majorityElement(nums);
 cout<<ans;
-}
+}*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int frequency=1;
+        int ans=nums[0];
+        
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1]){
+                frequency++;
+            }
+            else{
+                frequency=1;
+                ans=nums[i];
+            }
+         if(frequency>nums.size()/2){
+            return ans;
+        }
+       
+        }
+         return ans;
+    }
+    
+   
+};
+
+
+int main(){
+    Solution s;
+vector<int> nums={2,2,1,1,1,1,2};
+int ans=s.majorityElement(nums);
+cout<<ans;}
+
