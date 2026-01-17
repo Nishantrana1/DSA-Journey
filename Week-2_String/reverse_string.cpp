535d74c0a910453fb71077b72dc7d3e8da560b7a
Nishant Rana <94187nishant@gmail.com>
2026-01-17 12:04:32 +0530

    reverse string using advance strings

--- Week-2_String/reverse_string.cpp
+++ Week-2_String/reverse_string.cpp
@@ -1,16 +1,25 @@
 #include<iostream>
 #include<cstring>
+#include<algorithm>
 using namespace std;
-
+/*
 int main(){
     char str[]="Hello";
     int st=0;
     int end = strlen(str)-1;
-
+    
     while(st<end){
         swap(str[st],str[end]);
         st++;
         end--;
     }
     cout<<str<<" "<<endl;
+}
+*/
+
+int main(){
+    string str="Nishant";
+    reverse(str.begin(),str.end());
+    cout<<str;
+
 }
