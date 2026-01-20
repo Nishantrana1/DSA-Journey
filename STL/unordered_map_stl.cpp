2f612873ec7f9201f724fc476a5e7bc89abbaf56
Nishant Rana <94187nishant@gmail.com>
2026-01-20 23:22:55 +0530

    try some function of multimaping

---
+++ STL/unordered_map_stl.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<map>
+#include<unordered_map>
+using namespace std;
+
+int main(){
+    unordered_map<string,int> m;
+    m["tv"]=100;
+    m["laptop"]=100;
+    m["Phone"]=400;
+    m["Dryer"]=150;
+    m["Heater"]=200;
+    m.erase("tv");
+for(auto p: m){
+    cout<<p.first<<" "<<p.second<<endl;
+}
+if(m.find("tv") != m.end()){
+    cout<<"found"<<endl;
+}
+else {
+    cout<<"not found";
+}
+
+}
