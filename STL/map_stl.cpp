16ecdb5476c75bd068e8fa2dfd860895bcc3eebe
Nishant Rana <94187nishant@gmail.com>
2026-01-20 22:26:34 +0530

    try some map function

--- STL/map_stl.cpp
+++ STL/map_stl.cpp
@@ -10,11 +10,19 @@ int main(){
 
     m.insert({"camera",500});
     m.emplace("laptop",450);
+    m.erase("tv");
+
 
     map<string,int>::iterator it;
     for(it=m.begin();it != m.end();it++){
         cout<<it->first<<" "<<it->second<<endl;
     }
-    cout<<" count for laptop "<<m.count("laptop")<<endl;
-
+    cout<<"count for laptop "<<m.count("laptop")<<endl;
+    cout<<"value of camera"<<m["camera"]<<endl;
+    if(m.find("camera") != m.end()){
+        cout<<"found";
+    }
+    else{
+        cout<<"not found";
+    }
 }
