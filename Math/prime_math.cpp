9874129312614101fdd2deb0140690665515bb5f
Nishant Rana <94187nishant@gmail.com>
2026-01-24 23:12:50 +0530

    some fucking prime number code

--- Math/prime_math.cpp
+++ Math/prime_math.cpp
@@ -1,1 +1,23 @@
-#include<
+#include<iostream>
+using namespace std;
+
+void prime(int n){
+    for(int i = 2;i*i<=n;i++){
+
+        if(n%i==0){
+            cout<<"NOT prime number"<<endl;
+            return;
+        }
+        
+            
+        
+    }
+    cout<<"prime";
+}
+
+int main(){
+    int n;
+    cout<<"Enter a Number: ";
+    cin>>n;
+    prime(n);
+}
