64ed2b81ba8bbf5542443b17a92915319c717246
Nishant Rana <94187nishant@gmail.com>
2026-01-24 23:49:52 +0530

    some happy number

---
+++ Math/happy_number.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while (n != 1) {
+            if (seen.count(n))
+                return false;
+
+            seen.insert(n);
+
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return true;
+    }
+};
+
+
+int main(){
+    Solution s;
+    int n=20;
+    cout<<s.isHappy(n)<<endl;
+
+}
