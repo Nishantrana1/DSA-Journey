fca4e80de0f29e09dbdecaaed5df976b1724ebef
Nishant Rana <94187nishant@gmail.com>
2026-01-25 23:57:45 +0530

    solve Valid Anagram problem

---
+++ STL/leecode_q1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        unordered_map<char, int> mp;
+
+        for (char c : s)
+            mp[c]++;
+
+        for (char c : t) {
+            mp[c]--;
+            if (mp[c] < 0)
+                return false;
+        }
+    return true;
+    }
+
+};
