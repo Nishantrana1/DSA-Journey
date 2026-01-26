1d3f566e5cae7aa6817134dc9232ea54a2cba650
Nishant Rana <94187nishant@gmail.com>
2026-01-26 22:29:15 +0530

    binary search code

--- Math/README.md
+++ Math/README.md
@@ -4,3 +4,7 @@ Day 2 of doing random math logic building
   second one is digit in number 
   3rd one is happy number ( very hard it need unordered sent)
   i also solve one problem today 
+  
+
+
+  
---
+++ PHASE 4-Searching and Sorting/binary_search.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+vector<int> arr = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+int target=11;
+for(int i=0;i<arr.size();i++){
+    if(arr[i]==target){
+        cout<<"Element Found"<<i<<endl;
+    }
+    
+    
+}
+
+      
+    
+}
