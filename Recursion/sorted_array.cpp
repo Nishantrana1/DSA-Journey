ac48b0a9fe503c639a9307956d9e2d990ee08715
Nishant Rana <94187nishant@gmail.com>
2026-02-04 23:53:53 +0530

    sorted array qeustion

---
+++ Recursion/sorted_array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool sarr(vector<int>arr,int n){
+     if (n == 0 || n == 1)
+        return true;
+if(arr[n-1]<arr[n-2]){
+return false;
+}
+
+return sarr(arr,n-1);
+}
+
+int main(){
+    vector<int>vec={1,2,9,4,5};
+    int n=5;
+    cout<<sarr(vec,n);
+}
