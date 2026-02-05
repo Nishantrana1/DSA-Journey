a6cdeef4613b26111ac72c08fa16d3fd67ae7c6d
Nishant Rana <94187nishant@gmail.com>
2026-02-05 23:45:59 +0530

    main function

--- Recursion/binary_string_length.cpp
+++ Recursion/binary_string_length.cpp
@@ -1,21 +1,33 @@
+
+
 #include <iostream>
 using namespace std;
 
 void generateBinary(int index, int n, string &curr) {
-    // base case
+   
     if (index == n) {
         cout << curr << endl;
         return;
     }
 
-    // choice 1: put '0'
+    
     curr.push_back('0');
     generateBinary(index + 1, n, curr);
-    curr.pop_back();  // backtrack
+    curr.pop_back();  
 
-    // choice 2: put '1'
+   
     curr.push_back('1');
     generateBinary(index + 1, n, curr);
-    curr.pop_back();  // backtrack
+    curr.pop_back();  
 }
 
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    string curr = "";
+    generateBinary(0, n, curr);
+
+    return 0;
+}
