7b7cd608aa11ecf8ddf1cca5b05281255f1989cb
Nishant Rana <94187nishant@gmail.com>
2026-02-07 23:32:19 +0530

    made some changes in sort code

--- Recursion/sorted_array.cpp
+++ Recursion/sorted_array.cpp
@@ -3,17 +3,15 @@
 using namespace std;
 
 bool sarr(vector<int>arr,int n){
-if (n == 0 || n == 1)
+if(n==0||n==1){
     return true;
-if(arr[n-1]<arr[n-2]){
-return false;
 }
 
-return sarr(arr,n-1);
+return arr[n-1]>=arr[n-2]&&sarr(arr,n-1);
 }
 
 int main(){
-    vector<int>vec={1,2,9,4,5};
+    vector<int>vec={1,2,3,4,5};
     int n=5;
     cout<<sarr(vec,n);
 }
