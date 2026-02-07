e79599527188676200288b8797b0dfe206f571f7
Nishant Rana <94187nishant@gmail.com>
2026-02-07 23:48:30 +0530

    binary seach using recursion

---
+++ Recursion/binary_search.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool binary(vector<int> arr,int tar){
+    
+
+}
+
+int main(){
+    vector<int> arr={1,2,3,4,5,6,7,8,9};
+    int target=3;
+    cout<<binary(arr,target);
+}
