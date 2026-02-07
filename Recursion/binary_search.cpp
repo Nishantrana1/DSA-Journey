b70fd694580aedee76aee52e880e39271fca62df
Nishant Rana <94187nishant@gmail.com>
2026-02-07 23:55:03 +0530

    half code

--- Recursion/binary_search.cpp
+++ Recursion/binary_search.cpp
@@ -2,13 +2,27 @@
 #include<vector>
 using namespace std;
 
-bool binary(vector<int> arr,int tar){
-    
+int binary(vector<int> arr,int tar, int n){
+    int st=0;
+    int end=n-1;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+            if(arr[mid]==tar)
+            return mid;
+            else if(arr[mid]<tar){
+                    st=mid+1;
+            }
+            else if(arr[mid]>tar){
+                end=mid-1;
+            }
+            st++;
+            end--;
+    }
 
 }
 
 int main(){
     vector<int> arr={1,2,3,4,5,6,7,8,9};
     int target=3;
-    cout<<binary(arr,target);
+    cout<<binary(arr,target,arr.size());
 }
