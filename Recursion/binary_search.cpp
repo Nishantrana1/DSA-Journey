96b623ab24a113d412b54ac10dfecb3bb896275f
Nishant Rana <94187nishant@gmail.com>
2026-02-09 22:45:57 +0530

    correct working version

--- Recursion/binary_search.cpp
+++ Recursion/binary_search.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 
 int binary(vector<int> arr,int tar, int st,int end){
-    int st=0;
-    int end=n-1;
+    int mid=st+(end-st)/2;
+
     if(tar==arr[mid]){
         return mid;
     }
-    if(tar<arr[mid]){
+    else if(tar<arr[mid]){
        
-        return binary(arr,tar,st,end=mid-1)
+        return binary(arr,tar,st,end=mid-1);
     }
-    if(tar>arr[mid]){
+    else if(tar>arr[mid]){
         
-        return binary(arr,tar,st=mid+1,end)
+        return binary(arr,tar,st=mid+1,end);
     }
 
 }
 
 int main(){
     vector<int> arr={1,3,5,7,9,15,21,25,39,55};
-    int target=39;
+    int target=3;
     int st=0,end=arr.size()-1;
     cout<<binary(arr,target,st,end);
+    
 }
