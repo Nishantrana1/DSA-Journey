407d591cfc4f82d9fb00e7cc8e62514eb5b7332f
Nishant Rana <94187nishant@gmail.com>
2026-02-09 22:39:24 +0530

    binary search using recursion

--- Recursion/binary_search.cpp
+++ Recursion/binary_search.cpp
@@ -2,19 +2,19 @@
 #include<vector>
 using namespace std;
 
-int binary(vector<int> arr,int tar, int n){
+int binary(vector<int> arr,int tar, int st,int end){
     int st=0;
     int end=n-1;
-    while(st<=end){
-        int mid=st+(end-st)/2;
-            if(arr[mid]==tar)
-            return mid;
-            else if(arr[mid]<tar){
-                    st=mid+1;
-            }
-            else if(arr[mid]>tar){
-                end=mid-1;
-            }
+    if(tar==arr[mid]){
+        return mid;
+    }
+    if(tar<arr[mid]){
+       
+        return binary(arr,tar,st,end=mid-1)
+    }
+    if(tar>arr[mid]){
+        
+        return binary(arr,tar,st=mid+1,end)
     }
 
 }
@@ -22,5 +22,6 @@ int binary(vector<int> arr,int tar, int n){
 int main(){
     vector<int> arr={1,3,5,7,9,15,21,25,39,55};
     int target=39;
-    cout<<binary(arr,target,arr.size());
+    int st=0,end=arr.size()-1;
+    cout<<binary(arr,target,st,end);
 }
