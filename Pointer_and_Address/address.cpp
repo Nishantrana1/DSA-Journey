c128456a2e13fec2e2867023b124f444a2f57a7a
Nishant Rana <94187nishant@gmail.com>
2026-02-10 11:49:37 +0530

    understand basic of pointer

--- Pointer_and_Address/address.cpp
+++ Pointer_and_Address/address.cpp
@@ -4,5 +4,10 @@ using namespace std;
 int main(){
     int x=19;
     cout<<"Value of X : "<<x<<endl;
-    cout<<&x;
+    cout<<&x<<endl;
+
+    int* p=&x;
+    cout<<p<<endl;
+    cout<<&p<<endl;
+    cout<<*p<<endl;
 }
