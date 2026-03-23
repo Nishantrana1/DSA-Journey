953964a22f6c8402dfe6bcb968788ec2f31e92de
Nishant Rana <94187nishant@gmail.com>
2026-03-23 18:06:37 +0530

    2d array

--- Pointer_and_Address/pointer.cpp
+++ Pointer_and_Address/pointer.cpp
@@ -1,1 +1,14 @@
-#include<
+#include<iostream>
+
+using namespace std;
+
+void update(int* p){
+    *p=10;
+}
+int main(){
+    int x=7;
+    cout<<"Value before changing: "<<x<<endl;
+    update(&x);
+    cout<<"Value after changing: "<<x<<endl;
+    return 0;
+}
---
+++ Week-1_Vectors/2D_array.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int matrix[4][2]={{1,2},{3,4},{5,6},{7,8}};
+  
+    cout<<matrix[1][0];
+}
