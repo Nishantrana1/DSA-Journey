333b8eed2630f14e0c7a5d90ac8943508b725f15
Nishant Rana <94187nishant@gmail.com>
2026-03-23 18:27:44 +0530

    max sum in row

--- Week-1_Vectors/2D_array.cpp
+++ Week-1_Vectors/2D_array.cpp
@@ -2,6 +2,37 @@
 using namespace std;
 int main(){
     int matrix[4][2]={{1,2},{3,4},{5,6},{7,8}};
+    int row=4;
+    int col=2;
+    int target = 6;
   
-    cout<<matrix[1][0];
+   /*
+   for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+        
+    if(matrix[i][j]==target){
+        cout<<i<<j<<endl;
+    }
 }
+
+}
+*/
+int sum=0;
+int high=0;
+ for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+        sum=sum+matrix[i][j];
+       
+        }
+         if(high<sum){
+            high=sum;
+        
+    }
+    sum=0;
+}
+cout<<high;
+
+
+}
+
+
